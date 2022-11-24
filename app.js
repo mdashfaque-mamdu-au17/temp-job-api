1cad546eb3e8b78b17e47054072bf007743aca4c
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-24 10:35:19 +0530

    start script is changed

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(xss());
 
 // extra packages
 
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
